e586cb7a35e0c6072a845769be2de4eba258c1e0
Mateusz Jasnowski <mateuszjasnowski99@gmail.com>
2023-02-01 10:23:08 +0100

    catching cmd arguments

--- app.py
+++ app.py
@@ -1,24 +1,44 @@
+#!/bin/python
 """
 Codac assigment task
 Data processing app with PySpark usage
 author: Mateusz Jasnowski
 github: github.com/mateuszjasnowski/pyspark_codac_assigment_task
 """
+from argparse import ArgumentParser
+
 from app import LOGGER
 
-def main(first_dataset: str, second_dataset: str, filter: dict={}):
-    pass
+def main(first_dataset: str, second_dataset: str, country: list=[]):
+    LOGGER.debug(f"{first_dataset} {second_dataset} {country}")
 
 if __name__ == "__main__":
     """
     Start program if package have been called directly
     Catch given args and execute main().
+    Required args:
+    first_ds: str
+    second_ds: str
+    Optional args:
+    -c, --country: str
+    -h, --help: None
     """
-    LOGGER.debug("Test warning")
+    parser = ArgumentParser()
+    parser.add_argument("first_ds", type=str, help="Path to first dataset")
+    parser.add_argument("second_ds", type=str, help="Path to second dataset")
 
-    #TODO catch args
-    first_file = 'source_data\dataset_one.csv'
-    second_file = 'source_data\dataset_two.csv'
+    parser.add_argument(
+        "-c",
+        "--country",
+        action='append',
+        help="Filter by countries (Can be used multiple times)"
+    )
 
-    LOGGER.debug(f"Frist file: {first_file}")
-    LOGGER.debug(f"Second file: {second_file}")
+    args = parser.parse_args()
+
+    LOGGER.info(f"Recived {len(args.__dict__)} arguments: {args.__dict__}")
+
+    if args.country:
+        main(args.first_ds, args.second_ds, args.country)
+    else:
+        main(args.first_ds, args.second_ds)
