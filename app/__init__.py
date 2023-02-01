6b27179b27888c35c619be262fbf059deb994afc
Mateusz Jasnowski <mateuszjasnowski99@gmail.com>
2023-02-01 10:28:41 +0100

    creating log dir if not exists

--- app/__init__.py
+++ app/__init__.py
@@ -1,25 +1,21 @@
+"""
+App initial file
+"""
 import logging
+import os
 
-from logging.handlers import TimedRotatingFileHandler, RotatingFileHandler
+from logging.handlers import RotatingFileHandler
 
 #logging settings
 LOGGER = logging.getLogger(__name__)
 LOGGER.setLevel(logging.DEBUG) #TODO change to INFO if DEBUG not needed
 
 _log_formatter = logging.Formatter('%(asctime)s;%(levelname)s;%(message)s')
-_log_path = './logs/codac_assigment_log.log'
-#log_filehandler = logging.FileHandler('./logs/codac_assigment_log.log')
-
-'''log_day_rotating_handler = TimedRotatingFileHandler(
-    filename = _log_path,
-    when='D',
-    interval=5,
-    backupCount=5,
-)
-log_day_rotating_handler.setFormatter(_log_formatter)'''
+_LOG_PATH = './logs/codac_assigment_log.log'
+os.makedirs(os.path.dirname(_LOG_PATH), exist_ok=True)
 
 log_size_rotating_handler = RotatingFileHandler(
-    filename=_log_path,
+    filename=_LOG_PATH,
     mode='a',
     maxBytes=100*10,
     backupCount=5
@@ -27,4 +23,3 @@ log_size_rotating_handler = RotatingFileHandler(
 log_size_rotating_handler.setFormatter(_log_formatter)
 
 LOGGER.addHandler(log_size_rotating_handler)
-
