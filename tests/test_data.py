31e4cb3835523765694a3606364e7a42ebca5abe
Mateusz Jasnowski <mateuszjasnowski99@gmail.com>
2023-02-10 09:44:07 +0100

    init test files

---
+++ tests/test_data.py
@@ -0,0 +1,11 @@
+""" Test module of app.Data module """
+import pytest
+
+from . import SPARK
+import chispa
+
+class TestData():
+    """ Testing class to test Data module """
+
+    def test_data_init(self):
+        pass
