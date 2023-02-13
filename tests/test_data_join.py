a13597788b43d888645f790f4445e645327688f3
Mateusz Jasnowski <mateuszjasnowski99@gmail.com>
2023-02-13 09:59:54 +0100

    Data.join_data() tests

--- tests/test_data_join.py
+++ tests/test_data_join.py
@@ -8,11 +8,32 @@ class TestDataInit():
     """ Testing class to test Data.join_data() method """
 
     def test_data_join_correct_join_order(self):
-        """ Testing join_data() method """
+        """ Testing join_data() method joining 2 data sets in correct order"""
 
         test_object = Data(
             SPARK,
             "./tests/test_set/dataset_one.csv",
+            header=True
+        )
+
+        test_object.join_data(
+            "./tests/test_set/dataset_two.csv",
+            first_pk="id",
+            second_pk="id"
+            )
+
+        expected_result = SPARK.read.option("header", "true").csv(
+            "./tests/test_set/test_join_result_1.csv"
+            )
+
+        chispa.assert_df_equality(test_object.data, expected_result)
+
+    def test_data_join_wrong_order(self):
+        """ Testing join_data() method joining 2 data sets in wrong order"""
+
+        test_object = Data(
+            SPARK,
+            "./tests/test_set/dataset_two.csv",
             header=True
         )
 
@@ -23,9 +44,7 @@ class TestDataInit():
             )
 
         expected_result = SPARK.read.option("header", "true").csv(
-            "./tests/test_set/test_filter_result_5.csv"
+            "./tests/test_set/test_join_result_2.csv"
             )
 
         chispa.assert_df_equality(test_object.data, expected_result)
-
-
