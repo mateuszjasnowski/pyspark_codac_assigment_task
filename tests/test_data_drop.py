8c4a12f1049003aa95f6166023a9c2c0fbc91903
Mateusz Jasnowski <mateuszjasnowski99@gmail.com>
2023-02-13 17:50:30 +0100

    test optimalization with fixture

--- tests/test_data_drop.py
+++ tests/test_data_drop.py
@@ -1,5 +1,6 @@
 """ Test module of app.data.Data.drop_column() method"""
 import chispa
+import pytest
 
 from tests import SPARK
 from app.data import Data
@@ -8,44 +9,43 @@ from app.data import Data
 class TestDataDropColumn:
     """Testing class to test Data.drop_column() method"""
 
-    def test_data_drop_single_column(self):
+    @pytest.fixture()
+    def dataset_object(self):
+        """Creating Data object avalible for class's methods"""
+        return Data(SPARK, "./tests/test_set/dataset_one.csv", header=True)
+
+    def test_data_drop_single_column(self, dataset_object):
         """Testing Data.drop_column() dropping single column"""
 
-        test_object = Data(SPARK, "./tests/test_set/dataset_one.csv", header=True)
-
-        test_object.drop_column(["last_name"])
+        dataset_object.drop_column(["last_name"])
 
         expected_result = SPARK.read.option("header", "true").csv(
             "./tests/test_set/test_drop_result_1.csv"
         )
 
-        chispa.assert_df_equality(test_object.data, expected_result)
+        chispa.assert_df_equality(dataset_object.data, expected_result)
 
-    def test_data_drop_with_ignoring_one(self):
+    def test_data_drop_with_ignoring_one(self, dataset_object):
         """
         Testing Data.drop_column() dropping single column with given 2
         (one need to be ignored)
         """
 
-        test_object = Data(SPARK, "./tests/test_set/dataset_one.csv", header=True)
-
-        test_object.drop_column(["no_column", "last_name"])
+        dataset_object.drop_column(["no_column", "last_name"])
 
         expected_result = SPARK.read.option("header", "true").csv(
             "./tests/test_set/test_drop_result_1.csv"
         )
 
-        chispa.assert_df_equality(test_object.data, expected_result)
+        chispa.assert_df_equality(dataset_object.data, expected_result)
 
-    def test_data_drop_more_columns(self):
+    def test_data_drop_more_columns(self, dataset_object):
         """Testing Data.drop_column() dropping more than one column"""
 
-        test_object = Data(SPARK, "./tests/test_set/dataset_one.csv", header=True)
-
-        test_object.drop_column(["last_name", "first_name"])
+        dataset_object.drop_column(["last_name", "first_name"])
 
         expected_result = SPARK.read.option("header", "true").csv(
             "./tests/test_set/test_drop_result_2.csv"
         )
 
-        chispa.assert_df_equality(test_object.data, expected_result)
+        chispa.assert_df_equality(dataset_object.data, expected_result)
--- tests/test_data_rename.py
+++ tests/test_data_rename.py
@@ -1,5 +1,6 @@
 """ Test module of app.data.Data.rename_columns() method"""
 import chispa
+import pytest
 
 from tests import SPARK
 from app.data import Data
@@ -8,41 +9,40 @@ from app.data import Data
 class TestDataRenameColumns:
     """Testing class to test Data.rename_columns() method"""
 
-    def test_data_rename_correct_scenario(self):
+    @pytest.fixture()
+    def dataset_object(self):
+        """Setting up Data object for test"""
+        return Data(SPARK, "./tests/test_set/dataset_one.csv", header=True)
+
+    def test_data_rename_correct_scenario(self, dataset_object):
         """Testing Data.rename_columns() successful renaming with given all needed column names"""
 
-        test_object = Data(SPARK, "./tests/test_set/dataset_one.csv", header=True)
-
-        test_object.rename_columns(["col1", "col2", "col3", "col4", "col5"])
+        dataset_object.rename_columns(["col1", "col2", "col3", "col4", "col5"])
 
         expected_result = SPARK.read.option("header", "true").csv(
             "./tests/test_set/test_rename_result_1.csv"
         )
 
-        chispa.assert_df_equality(test_object.data, expected_result)
+        chispa.assert_df_equality(dataset_object.data, expected_result)
 
-    def test_data_rename_missing_column(self):
+    def test_data_rename_missing_column(self, dataset_object):
         """Testing Data.rename_columns() with missing one new colum name"""
 
-        test_object = Data(SPARK, "./tests/test_set/dataset_one.csv", header=True)
-
-        test_object.rename_columns(["col1", "col2", "col3", "col4"])
+        dataset_object.rename_columns(["col1", "col2", "col3", "col4"])
 
         expected_result = SPARK.read.option("header", "true").csv(
             "./tests/test_set/dataset_one.csv"
         )
 
-        chispa.assert_df_equality(test_object.data, expected_result)
+        chispa.assert_df_equality(dataset_object.data, expected_result)
 
-    def test_data_rename_too_many_new_columns(self):
+    def test_data_rename_too_many_new_columns(self, dataset_object):
         """Testing Data.rename_columns() with too much given columns"""
 
-        test_object = Data(SPARK, "./tests/test_set/dataset_one.csv", header=True)
-
-        test_object.rename_columns(["col1", "col2", "col3", "col4", "col5", "col6"])
+        dataset_object.rename_columns(["col1", "col2", "col3", "col4", "col5", "col6"])
 
         expected_result = SPARK.read.option("header", "true").csv(
             "./tests/test_set/dataset_one.csv"
         )
 
-        chispa.assert_df_equality(test_object.data, expected_result)
+        chispa.assert_df_equality(dataset_object.data, expected_result)
