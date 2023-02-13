1826cee2e63a0d4e8f2620c93b9bdf330cb0d482
Mateusz Jasnowski <mateuszjasnowski99@gmail.com>
2023-02-13 10:45:04 +0100

    pylint + black reformating and status update

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,5 +1,5 @@
 """ Inizializing tests """
 from pyspark.sql import SparkSession
 
-#Declaring SparkSession for Chispa testing
+# Declaring SparkSession for Chispa testing
 SPARK = SparkSession.builder.master("local").appName("ChispaTest").getOrCreate()
--- tests/test_data_join.py
+++ tests/test_data_join.py
@@ -4,47 +4,36 @@ import chispa
 from tests import SPARK
 from app.data import Data
 
-class TestDataJoinData():
-    """ Testing class to test Data.join_data() method """
+
+class TestDataJoinData:
+    """Testing class to test Data.join_data() method"""
 
     def test_data_join_correct_join_order(self):
-        """ Testing join_data() method joining 2 data sets in correct order"""
+        """Testing join_data() method joining 2 data sets in correct order"""
 
-        test_object = Data(
-            SPARK,
-            "./tests/test_set/dataset_one.csv",
-            header=True
-        )
+        test_object = Data(SPARK, "./tests/test_set/dataset_one.csv", header=True)
 
         test_object.join_data(
-            "./tests/test_set/dataset_two.csv",
-            first_pk="id",
-            second_pk="id"
-            )
+            "./tests/test_set/dataset_two.csv", first_pk="id", second_pk="id"
+        )
 
         expected_result = SPARK.read.option("header", "true").csv(
             "./tests/test_set/test_join_result_1.csv"
-            )
+        )
 
         chispa.assert_df_equality(test_object.data, expected_result)
 
     def test_data_join_wrong_order(self):
-        """ Testing join_data() method joining 2 data sets in wrong order"""
+        """Testing join_data() method joining 2 data sets in wrong order"""
 
-        test_object = Data(
-            SPARK,
-            "./tests/test_set/dataset_two.csv",
-            header=True
-        )
+        test_object = Data(SPARK, "./tests/test_set/dataset_two.csv", header=True)
 
         test_object.join_data(
-            "./tests/test_set/dataset_one.csv",
-            first_pk="id",
-            second_pk="id"
-            )
+            "./tests/test_set/dataset_one.csv", first_pk="id", second_pk="id"
+        )
 
         expected_result = SPARK.read.option("header", "true").csv(
             "./tests/test_set/test_join_result_2.csv"
-            )
+        )
 
         chispa.assert_df_equality(test_object.data, expected_result)
