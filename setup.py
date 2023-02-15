abfbdb7be4bbe31b14b246055fa2f7786b05571e
Mateusz Jasnowski <mateuszjasnowski99@gmail.com>
2023-02-15 12:01:56 +0100

    adding build wheel package to pipeline

--- setup.py
+++ setup.py
@@ -2,17 +2,17 @@
 from setuptools import setup
 
 with open("requirements.txt", mode='r', encoding='utf-8') as requirements:
-    requirements.readlines()
+    requirements = requirements.read()
 
 setup(
     name="CodacApp",
     author="Mateusz Jasnowski",
     author_email="mateusz.jasnowski@capgemini.com",
-    version="0.1.7",
+    version="0.2.0",
     description="PySpark application as upskilling task",
     packages=['codac_app','codac_app.app'],
+    install_requires = requirements,
     entry_points = {
         'console_scripts': ['codac-app=codac_app.__main__:app_start'],
     }
-    #TODO setup.py need to install requirements
 )
